5904019e0ebfd3c7bcd2995ae698432cb6fd2b40
Leah Wasser <leah.wasser@colorado.edu>
2019-09-02 14:53:26 -0600

    set index

--- docs/conf.py
+++ docs/conf.py
@@ -26,8 +26,8 @@ release = 'beta'
 
 
 # -- General configuration ---------------------------------------------------
-
-
+# index is the landing page
+master_doc = 'index'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
